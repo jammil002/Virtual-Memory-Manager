a4cba3635dce51226b111962abaf5db5a586dc34
James Miller <jam1211miller@gmail.com>
2023-03-26 18:46:38 -0700

    Commented code and worked on docs

--- vmm.cpp
+++ vmm.cpp
@@ -6,19 +6,19 @@
 // Functions
 void loadFiles();
 void initPageTable();
-std::string allocatePage(int logical_address);
+std::string allocatePage(int logicalAddress);
 void writeToOutputFile(std::string output);
 
 // Global variables and constants
-const int PAGE_SIZE = 4;
-const int LOGICAL_MEMORY_SIZE = 16;
-const int PHYSICAL_MEMORY_SIZE = 32;
-const int PAGE_TABLE_SIZE = LOGICAL_MEMORY_SIZE;
-int pageTable[PAGE_TABLE_SIZE];
-std::string logicalMemory[LOGICAL_MEMORY_SIZE][PAGE_SIZE];
-std::string physicalMemory[PHYSICAL_MEMORY_SIZE / PAGE_SIZE][PAGE_SIZE];
-unsigned long accessCounter = 0;
-unsigned long lastAccess[PHYSICAL_MEMORY_SIZE / PAGE_SIZE];
+const int PAGE_SIZE = 4;                                                 // The page size is 4
+const int LOGICAL_MEMORY_SIZE = 16;                                      // The logical memory size is 16
+const int PHYSICAL_MEMORY_SIZE = 32;                                     // The physical memory size is 32
+const int PAGE_TABLE_SIZE = LOGICAL_MEMORY_SIZE;                         // The page table size is the same as the logical memory size
+int pageTable[PAGE_TABLE_SIZE];                                          // The page table is an array of integers
+std::string logicalMemory[LOGICAL_MEMORY_SIZE][PAGE_SIZE];               // The logical memory is an array of strings
+std::string physicalMemory[PHYSICAL_MEMORY_SIZE / PAGE_SIZE][PAGE_SIZE]; // The physical memory is an array of strings
+unsigned long accessCounter = 0;                                         // This variable is used to keep track of the last access time of the frames
+unsigned long lastAccess[PHYSICAL_MEMORY_SIZE / PAGE_SIZE];              // This array is used to store the last access time of the frames
 
 // The page table is initialized by this function by setting all entries to 1.
 void initPageTable()
@@ -43,7 +43,7 @@ void loadFiles()
             offset = 0;
             pageIndex++;
         }
-        logicalMemory[pageIndex][offset] = line;
+        logicalMemory[pageIndex][offset] = line; // Store the line in the logical memory
         offset++;
     }
 }
@@ -73,11 +73,11 @@ int freeFrames()
         {
             if (physicalMemory[i][j].empty())
             {
-                isMemoryFree = false;
+                isMemoryFree = false; // If the frame is not empty, then it is not free
                 break;
             }
         }
-        if (isMemoryFree)
+        if (isMemoryFree) // If the frame is free, then return the index of the frame
         {
             return i;
         }
@@ -89,11 +89,11 @@ int freeFrames()
 int findLeastRecentlyUsedFrame()
 {
     int minIndex = 0;
-    unsigned long minValue = std::numeric_limits<unsigned long>::max();
+    unsigned long minValue = std::numeric_limits<unsigned long>::max(); // Set the minimum value to the maximum value of an unsigned long
 
     for (int i = 0; i < PHYSICAL_MEMORY_SIZE / PAGE_SIZE; i++)
     {
-        if (lastAccess[i] < minValue)
+        if (lastAccess[i] < minValue) // If the last access time of the frame is less than the minimum value, then update the minimum value and the index of the frame
         {
             minValue = lastAccess[i];
             minIndex = i;
@@ -103,10 +103,10 @@ int findLeastRecentlyUsedFrame()
 }
 
 // This function allocates a page to the physical memory and returns the string that is allocated to the frame.
-std::string allocatePage(int logical_address)
+std::string allocatePage(int logicalAddress)
 {
-    int pageIndex = logical_address / PAGE_SIZE;
-    int offset = logical_address % PAGE_SIZE;
+    int pageIndex = logicalAddress / PAGE_SIZE;
+    int offset = logicalAddress % PAGE_SIZE;
     int frameIndex = pageTable[pageIndex];
 
     std::string output = ""; // Create an empty string to store the output
@@ -115,13 +115,13 @@ std::string allocatePage(int logical_address)
     {
         frameIndex = freeFrames();
 
-        if (frameIndex == -1)
+        if (frameIndex == -1) // If there are no free frames, then find the least recently used frame
         {
-            frameIndex = findLeastRecentlyUsedFrame();
+            frameIndex = findLeastRecentlyUsedFrame(); // Find the least recently used frame
 
-            for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+            for (int i = 0; i < PAGE_TABLE_SIZE; i++) // Remove the page from the page table
             {
-                if (pageTable[i] == frameIndex)
+                if (pageTable[i] == frameIndex) // If the page is found in the page table, then remove it
                 {
                     pageTable[i] = -1;
                     break;
@@ -132,7 +132,7 @@ std::string allocatePage(int logical_address)
         pageTable[pageIndex] = frameIndex;
         for (int i = 0; i < PAGE_SIZE; i++)
         {
-            physicalMemory[frameIndex][i] = logicalMemory[pageIndex][i];
+            physicalMemory[frameIndex][i] = logicalMemory[pageIndex][i]; // Copy the page from the logical memory to the physical memory
         }
 
         std::string page_fault_msg = "Page fault occurred, loaded page " + std::to_string(pageIndex) + " into frame " + std::to_string(frameIndex);
@@ -140,8 +140,8 @@ std::string allocatePage(int logical_address)
         output += page_fault_msg + "\n"; // Append the message to the output string
     }
 
-    accessCounter++;
-    lastAccess[frameIndex] = accessCounter;
+    accessCounter++;                        // Increment the access counter
+    lastAccess[frameIndex] = accessCounter; // Update the last access time of the frame
 
     std::string outputMessage = "String: " + logicalMemory[pageIndex][offset] + " allocated to frame " + std::to_string(frameIndex);
     std::cout << outputMessage << std::endl;
